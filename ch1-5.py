baa065450ed8f9c297f370d8075f41650c76af18
chinyu88 <j88050160@gmail.com>
2020-05-04 23:13:33 +0800

    json父物件

--- ch1-5.py
+++ ch1-5.py
@@ -6,3 +6,12 @@ dictObj = json.loads(jsonObj)
 print(dictObj)
 print(type(dictObj))
 
+#一個json文件中指會有一個json物件
+#一次有多個json物件，可以用一個父json物件處理
+#Facebook資料以上此方式處理
+obj = '{"Asia" :[{"Japan":"Tokyo"},{"China":"Beiging"}]}'
+json_obj = json.loads(obj)
+print(json_obj)
+print(json_obj["Asia"])
+print(json_obj["Asia"][0])
+print(type(json_obj))
--- instagram_crawler_0410.py
+++ instagram_crawler_0410.py
@@ -1,7 +1,7 @@
 import urllib.request as r
 import json
-from bs4 import BeautifulSoup
-import jsonpath
+#from bs4 import BeautifulSoup
+#import jsonpath
 
 url = "https://www.instagram.com/explore/tags/taipeicafe/?__a=1"
 request = r.Request(url, headers={
@@ -29,3 +29,16 @@ dictObj = json.loads(jsonObj)
 origin_jsonObj = json.dumps(dictObj,sort_keys=True,indent=4)
 print("整理好的json")
 print(origin_jsonObj)
+dict_obj = json.loads(origin_jsonObj)
+print(dict_obj)
+print(type(dict_obj))
+picture_url = dict_obj["graphql"]
+
+#[hashtag][edge_hashtag_to_media][edges][node][display_url]
+print(picture_url)
+"""obj = '{"Asia" :[{"Japan":"Tokyo"},{"China":"Beiging"}]}'
+json_obj = json.loads(obj)
+print(json_obj)
+print(json_obj["Asia"])
+print(json_obj["Asia"][0])
+print(type(json_obj))"""
