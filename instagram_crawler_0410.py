4f2fdd0d29a878acaac5f8bb4ae81123fe3c9838
chinyu88 <j88050160@gmail.com>
2020-05-05 10:29:52 +0800

    json to dict， get url, download picture

--- instagram_crawler_0410.py
+++ instagram_crawler_0410.py
@@ -1,7 +1,10 @@
 import urllib.request as r
 import json
+import os
+import requests
 #from bs4 import BeautifulSoup
 #import jsonpath
+#開啟照片用
 
 url = "https://www.instagram.com/explore/tags/taipeicafe/?__a=1"
 request = r.Request(url, headers={
@@ -30,15 +33,18 @@ origin_jsonObj = json.dumps(dictObj,sort_keys=True,indent=4)
 print("整理好的json")
 print(origin_jsonObj)
 dict_obj = json.loads(origin_jsonObj)
-print(dict_obj)
+print("python_dict:",dict_obj)
 print(type(dict_obj))
-picture_url = dict_obj["graphql"]
+print("利用loads()將json轉成python的字典")
+print("再從字典中抓取照片網址")
+picture_url = dict_obj["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"][0]["node"]["display_url"]
 
 #[hashtag][edge_hashtag_to_media][edges][node][display_url]
 print(picture_url)
-"""obj = '{"Asia" :[{"Japan":"Tokyo"},{"China":"Beiging"}]}'
-json_obj = json.loads(obj)
-print(json_obj)
-print(json_obj["Asia"])
-print(json_obj["Asia"][0])
-print(type(json_obj))"""
+
+#下載圖片
+destDir = 'D:\git\photo/'
+img = requests.get(picture_url)
+#把jpg名稱改掉
+with open(destDir + 'a.jpg' , 'wb') as f:
+    f.write(img.content)
