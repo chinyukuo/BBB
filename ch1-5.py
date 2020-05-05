c42c93a05b33b80c4ea9a7bb13566160dff8a1ae
chinyu88 <j88050160@gmail.com>
2020-05-05 10:27:55 +0800

    json轉成python的dict，再從中抓取縮圖網址

--- ch1-5.py
+++ ch1-5.py
@@ -8,7 +8,7 @@ print(type(dictObj))
 
 #一個json文件中指會有一個json物件
 #一次有多個json物件，可以用一個父json物件處理
-#Facebook資料以上此方式處理
+#Facebook，IG資料以上此方式處理
 obj = '{"Asia" :[{"Japan":"Tokyo"},{"China":"Beiging"}]}'
 json_obj = json.loads(obj)
 print(json_obj)
