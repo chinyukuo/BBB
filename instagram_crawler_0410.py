b6ae3eee3279ce799c20b245d9073e6df87b9b4b
chinyu88 <j88050160@gmail.com>
2020-10-01 00:05:40 +0800

    0927 update 輸出

--- instagram_crawler_0410.py
+++ instagram_crawler_0410.py
@@ -7,20 +7,34 @@ import datetime
 #import jsonpath
 #開啟照片用
 
-url = "https://www.instagram.com/explore/tags/taipei/?__a=1"
+url = "https://www.instagram.com/explore/tags/chiayi/?__a=1"
 #取得hahtag名稱
 #可以再找更好方式
 spilt_url = url.split('/')
 hashtag = spilt_url[5]
+
+#更改地點
+def change_locate(locate_name):
+    global url
+    url = url.replace(hashtag, locate_name)
+
+change_locate(input('請輸入搜尋hashtag：'))
+print(url)
+
+spilt_url = url.split('/')
+hashtag = spilt_url[5]
+
+
 request = r.Request(url, headers={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"
 })
 
+
 html = r.urlopen(request)
 origin_html = html.read()
 
-print("未使用beautifulsoup的html")
-print(html)
+
+#print(html)
 
 with r.urlopen(request) as response:
     data = response.read().decode("utf-8")
@@ -28,6 +42,11 @@ with r.urlopen(request) as response:
 print("擷取到的json")
 print(data)
 
+#20200927嘗試輸出成檔案
+file = open("html.txt", "w")
+file.write(data)
+file.close()
+print('done')
 
 #2020/4/27
 jsonObj = origin_html
@@ -36,7 +55,7 @@ dictObj = json.loads(jsonObj)
 #print(type(dictObj))
 origin_jsonObj = json.dumps(dictObj,sort_keys=True,indent=4)
 print("整理好的json")
-print(origin_jsonObj)
+#print(origin_jsonObj)
 dict_obj = json.loads(origin_jsonObj)
 #2020/5/4
 print("python_dict:",dict_obj)
