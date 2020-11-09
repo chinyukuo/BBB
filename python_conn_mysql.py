ef33642e880a120d9202d09e5f4f0e96d9c308ff
chinyu88 <j88050160@gmail.com>
2020-11-09 10:57:50 +0800

    change python function for connect mysql database this is test for update new location

--- python_conn_mysql.py
+++ python_conn_mysql.py
@@ -1,22 +1,45 @@
+import mysql.connector
+from mysql.connector import Error
 
-#coding=utf-8
-import MySQLdb
+try:
+    # 連接 MySQL/MariaDB 資料庫
+    connection = mysql.connector.connect(
+        host='localhost',          # 主機名稱
+        database='bbb', # 資料庫名稱
+        user='root',        # 帳號
+        password='1234')  # 密碼
 
-conn = MySQLdb.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = '@3X.M&',
-    db ='websinfo_travelfun')
+    if connection.is_connected():
 
-cur = conn.cursor()
-#建立資料表
-#cur.execute("create table student(id int ,name varchar(20),class varchar(30),age varchar(10))")
-#插入一條資料
-cur.execute("insert into student values('2','Tom','3 year 2 class','9')")
-#修改查詢條件的資料
-#cur.execute("update student set class='3 year 1 class' where name = 'Tom'")
-#刪除查詢條件的資料
-#cur.execute("delete from student where age='9'")
-cur.close()
-conn.commit()
-conn.close()
+        # 顯示資料庫版本
+        db_Info = connection.get_server_info()
+        print("資料庫版本：", db_Info)
+
+
+        view_id = 30
+        view_name = "豐年公園"
+        #INSERT INTO `location` (`view_id`, `picture`, `view_name`) VALUES ('16', NULL, '復興崗');
+        sql = "INSERT INTO `location` (`view_id`, `picture`, `view_name`) VALUES (" + "'" + str(view_id) + "'" + ", NULL, "+"'"+ view_name + "'" + ");"
+        print(sql)
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+        sql = "SELECT * FROM `location`"
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        for (view_id,picture,view_name) in cursor:
+            print("view_id: %d" % view_id)
+            print("view_name: %s" % view_name)
+        
+        
+
+
+
+except Error as e:
+    print("資料庫連接失敗：", e)
+
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("資料庫連線已關閉")
